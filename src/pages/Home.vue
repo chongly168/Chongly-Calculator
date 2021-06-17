cba39febd0c9097f0e9034fea919556058368c23
chongly168 <doschongly92@gmail.com>
2021-06-18 01:09:03 +0700

    Dos Chongly-Calculator

---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Welcome Chongly</h1>
+    <h4>Project Vue Pertama Saya</h4>
+    <h4>Nama Saya {{ nama }}</h4>
+    <h4> Umur Saya {{ tahunlahir }} tahun</h4>
+    <input v-model="kotalahir" />
+    <h5>kota lahir saya : {{ kotalahir }} </h5>
+    <button v-on:click="next">next</button>
+</template>
+
+<script>
+export default {
+    data() {
+      return{
+          nama : "Dos Chongly",
+          tahunlahir : 1997,
+          kotalahir : "Phnom Penh, Kamboja"
+      }  
+    },
+    methods:{
+        next(){
+            this.tahunlahir += 1;
+        }
+    },
+    mounted(){
+        this.tahunlahir = 2021 - this.tahunlahir;
+    }
+}
+</script>
---
+++ src/pages/Loops.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>Loops</h1>
+<table class="center" border=1>
+    <tr v-for="hari in namaHari" v-bind:key="hari">
+        <td>{{ hari }} </td>
+        </tr>
+    </table>
+</template>
+
+
+
+<script>
+export default {
+        data(){
+            return{
+                namaHari : ["Senin", "Selasa", "Rabu", "Kamis",
+                            "Jumat", "Sabtu", "Minggu" ]
+            }
+        }
+    }
+</script>
+
+<style>
+.center{
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
